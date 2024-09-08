568692e863812e4c12c728a02092edf3d6964b46
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2024-09-09 01:57:01 +0530

    Math

--- Divisiblity_Problem.java
+++ Divisiblity_Problem.java
@@ -4,22 +4,15 @@ public class Divisiblity_Problem {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
      int t = sc.nextInt();
-     int [] arr = new int[t];
      for(int i = 0; i < t;i++){
+         int move = 0;
          int a = sc.nextInt();
          int b = sc.nextInt();
-         arr[i] = moves(a,b);
-     }
-     for(int i = 0;i < t;i++){
-         System.out.println(arr[i]);
-     }
-    }
-    public static int moves(int a,int b){
-        int move = 0;
-        while(a%b!=0){
-            move++;
-            a++;
+         if(a%b == 0){
+             System.out.println(move);
+         } else{
+             System.out.println((b-(a%b))%b);
+         }
         }
-        return move;
     }
 }
