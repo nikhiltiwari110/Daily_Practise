582d7c5cf61203035218d131bedd87931deff5b0
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2025-01-31 00:18:47 +0530

    Wrong Subtraction Codeforces

---
+++ Codeforces/Wrong_Subtraction.java
@@ -0,0 +1,20 @@
+package Daily_Practise.Codeforces;
+
+import java.util.Scanner;
+
+public class Wrong_Subtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int i = 1; i <= k; i++) {
+            int rem = n % 10;
+            if (rem == 0) {
+                n = n / 10;
+            }else {
+                n = n - 1;
+            }
+        }
+        System.out.println(n);
+    }
+}
