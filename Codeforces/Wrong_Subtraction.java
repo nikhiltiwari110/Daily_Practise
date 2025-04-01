d6081ff09d6a8c66b604548d1a5a566f77db3c1f
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2025-04-01 22:52:13 +0530

    Wrong Subtraction
    You are given a number n and k is the number of times you have to do subtraction. If the number last digit is non zero then subtract one and if zero divide the number by 10. Return the positive integer result after k times subtraction

--- Codeforces/Wrong_Subtraction.java
+++ Codeforces/Wrong_Subtraction.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Wrong_Subtraction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt();//number
         int k = sc.nextInt();
         for (int i = 1; i <= k; i++) {
             int rem = n % 10;
