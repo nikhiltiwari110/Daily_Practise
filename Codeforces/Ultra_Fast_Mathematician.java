d428c1f34dde49a2ef1540cad108878f7cff9b10
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2025-04-04 22:50:55 +0530

    Ultra fast mathematician
    xor the two nos

---
+++ Codeforces/Ultra_Fast_Mathematician.java
@@ -0,0 +1,18 @@
+package Daily_Practise.Codeforces;
+
+import java.util.Scanner;
+
+public class Ultra_Fast_Mathematician {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i)!=b.charAt(i)){
+                System.out.print("1");
+            }else {
+                System.out.print("0");
+            }
+        }
+    }
+}
