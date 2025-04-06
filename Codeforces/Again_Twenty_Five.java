02b6a0edb41aa1f0135ba604a68d87643aa2a667
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2025-04-06 21:58:43 +0530

    Last two digits
    You are given n . You have to tell 2^n last two digits

---
+++ Codeforces/Again_Twenty_Five.java
@@ -0,0 +1,11 @@
+package Daily_Practise.Codeforces;
+
+import java.util.Scanner;
+
+public class Again_Twenty_Five {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(25);
+    }
+}
