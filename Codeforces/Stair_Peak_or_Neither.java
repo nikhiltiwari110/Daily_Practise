ae4b9314630ab9241e5b5bb214cd507c69c83e49
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2025-04-07 19:32:31 +0530

    Stair , Peak or Neither
    if a < b < c then Stair
    if a < b > c then Peak
    otherwise None

---
+++ Codeforces/Stair_Peak_or_Neither.java
@@ -0,0 +1,22 @@
+package Daily_Practise.Codeforces;
+
+import java.util.Scanner;
+
+public class Stair_Peak_or_Neither {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t= sc.nextInt();
+        while (t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if (a<b && b<c){
+                System.out.println("STAIR");
+            } else if (a<b&&b>c) {
+                System.out.println("PEAK");
+            }else {
+                System.out.println("NONE");
+            }
+        }
+    }
+}
