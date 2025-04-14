1ca50393fa442f64ff0ed54dc58d79404092e4cd
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2025-04-14 21:14:22 +0530

    Who Won?
    Vanya and Vova are playing a game. Players are given an integer n. On their turn, the player can add 1 to the current integer or subtract 1 . The players take turns; Vanya starts. If after Vanya's move the integer is divisible by 3, then he wins. If 10 moves have passed and Vanya has not won, then Vova wins.
    Write a program that, based on the integer n, determines who will win if both players play optimally.
    For each test case, print "First" without quotes if Vanya wins, and "Second" without quotes if Vova wins.

---
+++ Codeforces/Game_With_Integers.java
@@ -0,0 +1,18 @@
+package Daily_Practise.Codeforces;
+
+import java.util.Scanner;
+
+public class Game_With_Integers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0){
+            int n = sc.nextInt();
+            if (n%3==0){
+                System.out.println("Second");
+            }else {
+                System.out.println("First");
+            }
+        }
+    }
+}
