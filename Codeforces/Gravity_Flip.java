2b990fb7334959f10de3850efec70cac7150f80a
nikhiltiwari110 <nikhil.tiwari1447@gmail.com>
2026-01-31 22:09:13 +0530

    Create Gravity_Flip.java

---
+++ Codeforces/Gravity_Flip.java
@@ -0,0 +1,19 @@
+package Daily_Practise.Codeforces;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Gravity_Flip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
